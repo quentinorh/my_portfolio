d2899af43701ec9e4947457ec43ce30205867cc5
Quentin Orhant <quentin.orhant@gmail.com>
2023-01-03 14:56:43 +0100

    add source and description

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,6 +36,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, :cover_url, photos: [], tag_list: [])
+    params.require(:post).permit(:title, :description, :source, photos: [], tag_list: [])
   end
 end
