b4c3b43b7b3ded6a6fefd27a2f70dec8e207e9b5
Quentin Orhant <quentin.orhant@gmail.com>
2023-01-10 18:16:44 +0100

    add nav next prev

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @related_posts = @post.find_related_tags
+    @posts = Post.all.order(order_number: :desc)
   end
 
   def new
